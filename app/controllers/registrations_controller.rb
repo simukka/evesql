220daf98a456ca8044cfb063303ce9dc188adb1e
kyle Simukka <kyle@janrain.com>
2013-01-30 16:23:50 -0800

    added better nav, added getting started

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
     render :edit
   end
   def api
+    authorize! :api, @user, :message => 'You need to sign in or sign up before continuing.'
     @user = current_user
     @current_usage = @user.apikey.usage_count.to_f
     @max_usage = @user.roles.first.max_usage.to_f
